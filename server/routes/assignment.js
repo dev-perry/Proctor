164b72cad448be31ec901ea055d29c802916fc1f
Perry Asibey-Bonsu <perry@CENTURION.local>
2020-02-01 23:11:11 -0500

    Routes for API calls related to assignments

---
+++ server/routes/assignment.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router(); //accesses router functions from express package
+ //accesses models and functions
+const Assign = require('../database/models/assignment');
+const User = require('../database/models/user.js');
+const passport = require('../passport');
+
+
+
+//Create a new Assignment
+router.post('/', function(req, res) {
+  if (req.isAuthenticated()) {
+    const newAssignment = new Assign({
+      course_id: req.body.course_id,
+      name: req.body.name,
+      type: req.body.type,
+      tags: req.body.tags,
+      visible: req.body.visible,
+      due: req.body.due
+    });
+
+    newAssignment.save(function(err, savedAssign) {
+      if (err) {
+        return res.json(err);
+      }
+      res.json(savedAssign);
+    })
+  } else {
+    res.send("Request denied");
+  }
+});
+
+//return the user's assignment calendar
+router.get('/calendar/:uid', function(req, res) {
+  User.findOne({_id:req.params.uid}, 'courses', function(err, docs){
+    if(err){
+      return err;
+    }else{
+      return docs;
+    }
+  }).then((response)=>{//after getting courseID array from user object, match documents in assignment collection
+      Assign.find({'course.id':{$in:response.courses}, visible: true}, 'name due tags course', function(err, docs){
+        if(err){
+          res.json(err);
+        }else{
+          res.json(docs);
+        }
+      });
+  })
+  .catch(function(error){
+    console.log('Error getting assignments:');
+    console.log(error);
+  })
+})
+
+module.exports = router;
