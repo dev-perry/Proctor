66bc8bf4268b6c67435e16cd63891185e313b0cd
Perry Asibey-Bonsu <perry@client-2-2-104.wireless.drew.lan>
2020-02-23 16:26:11 -0500

    Added submission posting API shell

--- client/src/components/blocks/ResArea.jsx
+++ client/src/components/blocks/ResArea.jsx
@@ -2,21 +2,37 @@ import React from "react";
 import TODOres from '../elements/TODOres.jsx';
 import SubRes from './SubRes.jsx';
 import QuizRes from './QuizRes.jsx';
+import axios from 'axios';
 
 function ResArea(props) {
+  const uID = props.uID;
+  const aID = props.aID;
   const type = props.amentType;
   let res = null;
 
+  function sendSubmission(data){
+    axios.post('http://localhost:4000/assignment/submission/'+ uID + '/' + aID, data)
+    .then( res => {
+      if(res.data){
+        console.log("Data caught");
+        console.log(res.data);
+      }else{
+        console.log("No data caught");
+      }
+    }
+    )
+  }
+
   switch (type) {
     case "TODO":
-      res = <TODOres/>
+      res = <TODOres sendSub = {sendSubmission}/>
       break;
     case "Submission":
-      res = <SubRes/>
+      res = <SubRes sendSub = {sendSubmission}/>
       break;
 
     case "Form":
-      res = <QuizRes/>
+      res = <QuizRes sendSub = {sendSubmission}/>
       break;
 
     default:
